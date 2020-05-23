c09259037d93f73ec06443619374ca53ad073aea
Martin Kulich <kulda.zbraslav@gmail.com>
2020-05-23 12:42:29 +0000

    Initial commit

---
+++ feed.php
@@ -0,0 +1,77 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!-- generator="FeedCreator 1.7.2-ppt DokuWiki" -->
+<?xml-stylesheet href="http://spl2009.instruktori.cz/lib/exe/css.php?s=feed" type="text/css"?>
+<rdf:RDF
+    xmlns="http://purl.org/rss/1.0/"
+    xmlns:rdf="http://www.w3.org/1999/02/22-rdf-syntax-ns#"
+    xmlns:slash="http://purl.org/rss/1.0/modules/slash/"
+    xmlns:dc="http://purl.org/dc/elements/1.1/">
+    <channel rdf:about="http://spl2009.instruktori.cz/feed.php">
+        <title>Spirit Pony Leadership</title>
+        <description></description>
+        <link>http://spl2009.instruktori.cz/</link>
+        <image rdf:resource="http://spl2009.instruktori.cz/lib/images/favicon.ico" />
+       <dc:date>2012-04-11T23:02:06+02:00</dc:date>
+        <items>
+            <rdf:Seq>
+                <rdf:li rdf:resource="http://spl2009.instruktori.cz/pro_koho_je_spl?rev=1280205771&amp;do=diff"/>
+                <rdf:li rdf:resource="http://spl2009.instruktori.cz/kontakty?rev=1280205534&amp;do=diff"/>
+                <rdf:li rdf:resource="http://spl2009.instruktori.cz/o_tymu?rev=1280205484&amp;do=diff"/>
+                <rdf:li rdf:resource="http://spl2009.instruktori.cz/start?rev=1280205407&amp;do=diff"/>
+            </rdf:Seq>
+        </items>
+    </channel>
+    <image rdf:about="http://spl2009.instruktori.cz/lib/images/favicon.ico">
+        <title>Spirit Pony Leadership</title>
+        <link>http://spl2009.instruktori.cz/</link>
+        <url>http://spl2009.instruktori.cz/lib/images/favicon.ico</url>
+    </image>
+    <item rdf:about="http://spl2009.instruktori.cz/pro_koho_je_spl?rev=1280205771&amp;do=diff">
+        <dc:format>text/html</dc:format>
+        <dc:date>2010-07-27T06:42:51+02:00</dc:date>
+        <dc:creator>ondra</dc:creator>
+        <title>Pro koho bylo SPL</title>
+        <link>http://spl2009.instruktori.cz/pro_koho_je_spl?rev=1280205771&amp;do=diff</link>
+        <description>PRO KOHO? Třeba pro ty z vás:
+
+	*  které téma leadershipu zajímá, případně se jím už aktivně zabývají
+
+	*  kteří hledají nové přístupy k vlastnímu rozvoji
+
+	*  kteří již mají organizátorskou zkušenost z přípravy zážitkové akce</description>
+    </item>
+    <item rdf:about="http://spl2009.instruktori.cz/kontakty?rev=1280205534&amp;do=diff">
+        <dc:format>text/html</dc:format>
+        <dc:date>2010-07-27T06:38:54+02:00</dc:date>
+        <dc:creator>ondra</dc:creator>
+        <title>Kontakty</title>
+        <link>http://spl2009.instruktori.cz/kontakty?rev=1280205534&amp;do=diff</link>
+        <description>S dotazy, přáními a stížnostmi se na nás obracejte:
+
+Email: kun (takovatazamotanavec) welbloud.cz 
+
+Telefon: 720 447 544 (Janka)</description>
+    </item>
+    <item rdf:about="http://spl2009.instruktori.cz/o_tymu?rev=1280205484&amp;do=diff">
+        <dc:format>text/html</dc:format>
+        <dc:date>2010-07-27T06:38:04+02:00</dc:date>
+        <dc:creator>ondra</dc:creator>
+        <title>O týmu SPL</title>
+        <link>http://spl2009.instruktori.cz/o_tymu?rev=1280205484&amp;do=diff</link>
+        <description>[Bakar .. nejdůležitější člen týmu]
+
+... a kromě Bakar se na vás těší mlok, Janka, Leňa s Lubošem, Dan, Palda a Athaj!</description>
+    </item>
+    <item rdf:about="http://spl2009.instruktori.cz/start?rev=1280205407&amp;do=diff">
+        <dc:format>text/html</dc:format>
+        <dc:date>2010-07-27T06:36:47+02:00</dc:date>
+        <dc:creator>ondra</dc:creator>
+        <title>Spirit Pony Leadership</title>
+        <link>http://spl2009.instruktori.cz/start?rev=1280205407&amp;do=diff</link>
+        <description>... aneb nachystali jsme pro vás, aktivní instruktory:
+
+	*  víkendovou akci, na které bude hlavním tématem leadership, aneb vedení lidí a práce s nimi. 
+
+	*  dny i noci programu, kde kromì osvìdèeného prostøedku -- hry -- budeme vyuívat i nìco netradièního - spolupráci s koòmi.</description>
+    </item>
+</rdf:RDF>
---
+++ feed8be0.php
@@ -0,0 +1,151 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!-- generator="FeedCreator 1.7.2-ppt DokuWiki" -->
+<?xml-stylesheet href="http://spl2009.instruktori.cz/lib/exe/css.php?s=feed" type="text/css"?>
+<rdf:RDF
+    xmlns="http://purl.org/rss/1.0/"
+    xmlns:rdf="http://www.w3.org/1999/02/22-rdf-syntax-ns#"
+    xmlns:slash="http://purl.org/rss/1.0/modules/slash/"
+    xmlns:dc="http://purl.org/dc/elements/1.1/">
+    <channel rdf:about="http://spl2009.instruktori.cz/feed.php">
+        <title>Spirit Pony Leadership</title>
+        <description></description>
+        <link>http://spl2009.instruktori.cz/</link>
+        <image rdf:resource="http://spl2009.instruktori.cz/lib/images/favicon.ico" />
+       <dc:date>2012-04-09T12:57:34+02:00</dc:date>
+        <items>
+            <rdf:Seq>
+                <rdf:li rdf:resource="http://spl2009.instruktori.cz/inspirace?rev=1241596837&amp;do=diff"/>
+                <rdf:li rdf:resource="http://spl2009.instruktori.cz/kontakty?rev=1280205534&amp;do=diff"/>
+                <rdf:li rdf:resource="http://spl2009.instruktori.cz/o_cem_to_ne_bude?rev=1241618573&amp;do=diff"/>
+                <rdf:li rdf:resource="http://spl2009.instruktori.cz/o_tymu?rev=1280205484&amp;do=diff"/>
+                <rdf:li rdf:resource="http://spl2009.instruktori.cz/org_lide?rev=1280044848&amp;do=diff"/>
+                <rdf:li rdf:resource="http://spl2009.instruktori.cz/prakticke_informace?rev=1241110945&amp;do=diff"/>
+                <rdf:li rdf:resource="http://spl2009.instruktori.cz/prihlaska?rev=1243767349&amp;do=diff"/>
+                <rdf:li rdf:resource="http://spl2009.instruktori.cz/pro_koho_je_spl?rev=1280205771&amp;do=diff"/>
+                <rdf:li rdf:resource="http://spl2009.instruktori.cz/sidebar?rev=1245833433&amp;do=diff"/>
+                <rdf:li rdf:resource="http://spl2009.instruktori.cz/start?rev=1280205407&amp;do=diff"/>
+            </rdf:Seq>
+        </items>
+    </channel>
+    <image rdf:about="http://spl2009.instruktori.cz/lib/images/favicon.ico">
+        <title>Spirit Pony Leadership</title>
+        <link>http://spl2009.instruktori.cz/</link>
+        <url>http://spl2009.instruktori.cz/lib/images/favicon.ico</url>
+    </image>
+    <item rdf:about="http://spl2009.instruktori.cz/inspirace?rev=1241596837&amp;do=diff">
+        <dc:format>text/html</dc:format>
+        <dc:date>2009-05-06T10:00:37+02:00</dc:date>
+        <title>Inspirace</title>
+        <link>http://spl2009.instruktori.cz/inspirace?rev=1241596837&amp;do=diff</link>
+        <description>... zážitkovou pedagogikou
+
+	*  Prázdninovka Spirit Horse, pořádaná částí týmu v létě 2008 &lt;http://spirithorse.instruktori.cz&gt;
+	*  Instruktoři Brno &lt;http://www.instruktori.cz&gt;
+
+... alternativním vedením k vedení
+
+	*  V Evropě: Horse Assisted Education  neboli Horse assisted therapy - &lt;http://www.eahae.org&gt; 
+	*  V USA: Equine Guided Education - &lt;http://www.equineguidededucation.org&gt;</description>
+    </item>
+    <item rdf:about="http://spl2009.instruktori.cz/kontakty?rev=1280205534&amp;do=diff">
+        <dc:format>text/html</dc:format>
+        <dc:date>2010-07-27T06:38:54+02:00</dc:date>
+        <title>Kontakty</title>
+        <link>http://spl2009.instruktori.cz/kontakty?rev=1280205534&amp;do=diff</link>
+        <description>S dotazy, přáními a stížnostmi se na nás obracejte:
+
+Email: kun (takovatazamotanavec) welbloud.cz 
+
+Telefon: 720 447 544 (Janka)</description>
+    </item>
+    <item rdf:about="http://spl2009.instruktori.cz/o_cem_to_ne_bude?rev=1241618573&amp;do=diff">
+        <dc:format>text/html</dc:format>
+        <dc:date>2009-05-06T16:02:53+02:00</dc:date>
+        <title>O čem (ne)bude SPL</title>
+        <link>http://spl2009.instruktori.cz/o_cem_to_ne_bude?rev=1241618573&amp;do=diff</link>
+        <description>O ČEM to NEBUDE:
+
+
+O čem může být trénink leadershipu?
+
+... sedím v učebně. Kolem mě dalších dvacet znuděných týpků přibližně
+mého věku. Mladí, nadějní, na startu slibné kariéry, budoucí top
+manažeři. Čekáme co se bude dít. „Musíte své spolupracovníky plně
+motivovat“, začíná nám vykládat chlápek v dokonale padnoucim obleku.
+Kecy, kecy, kecy,... Motivuje mě zcela plně k tomu, abych se po obědové 
+pauze už na školení nevracel...</description>
+    </item>
+    <item rdf:about="http://spl2009.instruktori.cz/o_tymu?rev=1280205484&amp;do=diff">
+        <dc:format>text/html</dc:format>
+        <dc:date>2010-07-27T06:38:04+02:00</dc:date>
+        <title>O týmu SPL</title>
+        <link>http://spl2009.instruktori.cz/o_tymu?rev=1280205484&amp;do=diff</link>
+        <description>[Bakar .. nejdůležitější člen týmu]
+
+... a kromě Bakar se na vás těší mlok, Janka, Leňa s Lubošem, Dan, Palda a Athaj!</description>
+    </item>
+    <item rdf:about="http://spl2009.instruktori.cz/org_lide?rev=1280044848&amp;do=diff">
+        <dc:format>text/html</dc:format>
+        <dc:date>2010-07-25T10:00:48+02:00</dc:date>
+        <title>Lide</title>
+        <link>http://spl2009.instruktori.cz/org_lide?rev=1280044848&amp;do=diff</link>
+        <description></description>
+    </item>
+    <item rdf:about="http://spl2009.instruktori.cz/prakticke_informace?rev=1241110945&amp;do=diff">
+        <dc:format>text/html</dc:format>
+        <dc:date>2009-04-30T19:02:25+02:00</dc:date>
+        <title>Praktické informace</title>
+        <link>http://spl2009.instruktori.cz/prakticke_informace?rev=1241110945&amp;do=diff</link>
+        <description>Akce SPIRIT PONY LEADERSHIP začne nedlouho po 18té hodině ve čtvrtek 18. 6. 2009. Konec je naplánován na 16tou hodinu v neděli 21. 6. 2009.
+
+Cena je předběžně stanovena na  1600 Kč. Cca 850 kč dáte za koně, zbytek pokryje ubytování, stravu, náklady na materiál a podobně. Informace o platbách nalezneš v informačním mejlu, který odešleme obratem po vyplnění přihlášky zde na webu.</description>
+    </item>
+    <item rdf:about="http://spl2009.instruktori.cz/prihlaska?rev=1243767349&amp;do=diff">
+        <dc:format>text/html</dc:format>
+        <dc:date>2009-05-31T12:55:49+02:00</dc:date>
+        <title>Přihláška</title>
+        <link>http://spl2009.instruktori.cz/prihlaska?rev=1243767349&amp;do=diff</link>
+        <description>Vyplněním a odesláním tohoto formuláře projevíte svuj zájem jet s námi na víkendovku Spirit Pony Leadership. Následně vás kontaktujeme skrz e-mail, vyplňte prosím proto svoji e-mailovou adresu pečlivě.  Pro odeslání je nutné vyplnit všechna pole formuláře.</description>
+    </item>
+    <item rdf:about="http://spl2009.instruktori.cz/pro_koho_je_spl?rev=1280205771&amp;do=diff">
+        <dc:format>text/html</dc:format>
+        <dc:date>2010-07-27T06:42:51+02:00</dc:date>
+        <title>Pro koho bylo SPL</title>
+        <link>http://spl2009.instruktori.cz/pro_koho_je_spl?rev=1280205771&amp;do=diff</link>
+        <description>PRO KOHO? Třeba pro ty z vás:
+
+	*  které téma leadershipu zajímá, případně se jím už aktivně zabývají
+
+	*  kteří hledají nové přístupy k vlastnímu rozvoji
+
+	*  kteří již mají organizátorskou zkušenost z přípravy zážitkové akce</description>
+    </item>
+    <item rdf:about="http://spl2009.instruktori.cz/sidebar?rev=1245833433&amp;do=diff">
+        <dc:format>text/html</dc:format>
+        <dc:date>2009-06-24T10:50:33+02:00</dc:date>
+        <title>sidebar</title>
+        <link>http://spl2009.instruktori.cz/sidebar?rev=1245833433&amp;do=diff</link>
+        <description>*  Intro
+	*  O čem (ne)bylo SPL
+	*  Inspirace
+
+	*  Pro koho bylo SPL
+	*  Praktické infomace
+	*  O týmu
+
+	*  Kontakty
+
+	*  Pro účastníky SPL09</description>
+    </item>
+    <item rdf:about="http://spl2009.instruktori.cz/start?rev=1280205407&amp;do=diff">
+        <dc:format>text/html</dc:format>
+        <dc:date>2010-07-27T06:36:47+02:00</dc:date>
+        <title>Spirit Pony Leadership</title>
+        <link>http://spl2009.instruktori.cz/start?rev=1280205407&amp;do=diff</link>
+        <description>... aneb nachystali jsme pro vás, aktivní instruktory:
+
+	*  víkendovou akci, na které bude hlavním tématem leadership, aneb vedení lidí a práce s nimi. 
+
+	*  dny i noci programu, kde kromì osvìdèeného prostøedku -- hry -- budeme vyuívat i nìco netradièního - spolupráci s koòmi.</description>
+    </item>
+</rdf:RDF>
